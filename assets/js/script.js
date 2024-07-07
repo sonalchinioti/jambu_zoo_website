de5f547e355af44c04b5fe2418a4fb1571aa193c
Sonal Vij <sonalchinioti@gmail.com>
2024-07-07 16:04:51 +0530

    reducing the time of loader icon.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@
 	//Hide Loading Box (Preloader)
 	function handlePreloader() {
 		if($('.loader-wrap').length){
-			$('.loader-wrap').delay(1000).fadeOut(500);
+			$('.loader-wrap').delay(1000).fadeOut(200);
 		}
 	}
 
